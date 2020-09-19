675ecf672df17ea35eb46a20582d991665135226
Emily Aguirre <emgal.aguirre@gmail.com>
2020-09-18 22:43:57 -0500

    created framework, real time not working

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+function realtimeClock() {
+
+    var rtClock = new Date();
+
+    var hour = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    // add AM and PM system
+    var amPm = (hours < 12) ? "AM" : "PM";
+
+    // Convert the hours components to 12-hour format
+    hours = (hours > 12) ? hour - 12 : hours;
+
+    //Pad the house. minutes and seconds with leading zeros
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //Display the clock 
+    document.getElementById('clock').innerHTML =
+        hours + " : " + minutes + "  :  " + seconds + " " + amPm;
+    var t = setTimeout(realtimeClock, 500);
+
+}
